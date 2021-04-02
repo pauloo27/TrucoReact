1c89eeba34c924575bf231b609866e584212a4d4
Pauloo27 <pauloeduardo2709@gmail.com>
2021-04-02 18:11:34 +0000

    Deploying to gh-pages from @ Pauloo27/TrucoReact@6ea11f10086f5d9716abd6ebb85accf8905c9eed 🚀

---
+++ precache-manifest.a2a4491f98b3ff6fcdd61a25014b0a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eaa9dd44b47cfaa586a71417ebdaab8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d15e7a1f93d7dd76c86",
+    "url": "/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "de84d87d6c0f4bbce5c8",
+    "url": "/static/css/main.bed3f5ff.chunk.css"
+  },
+  {
+    "revision": "0d15e7a1f93d7dd76c86",
+    "url": "/static/js/2.06a27392.chunk.js"
+  },
+  {
+    "revision": "de84d87d6c0f4bbce5c8",
+    "url": "/static/js/main.56a42f26.chunk.js"
+  },
+  {
+    "revision": "455ec4ecabdaec58e195",
+    "url": "/static/js/runtime-main.715729d4.js"
+  }
+]);
